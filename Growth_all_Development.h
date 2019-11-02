c8dbe2d3351c35d5964cac0af8e0c0278cddb280
Praefi <45772472+Praefi@users.noreply.github.com>
2019-11-03 00:50:46 +0100

    Create Growth_all_Development.h
    spliting topics

---
+++ Growth_all_Development.h
@@ -0,0 +1,26 @@
+
+#ifndef GROWTH_ALL_DEVELOPMENT_H
+#define GROWTH_ALL_DEVELOPMENT_H
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void basic_development (Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, Spielfeld, Special_Fields, Spielfeld, unsigned int*, Growth_Player*, unsigned int, unsigned int, unsigned int*, unsigned int*, unsigned int, unsigned int, unsigned int, unsigned int, Evolution, Num_num*, unsigned int*, Special_Fields, Limits, Single_option_representives, unsigned int*, unsigned int*, unsigned int, unsigned int);
+
+void new_life (Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int, Evolution, Spielfeld, Special_Fields, unsigned int);	//checklist
+void old_dying (Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int, Spielfeld, unsigned int, Special_Fields, Spielfeld, unsigned int);	//checklist
+
+
+void change (Spielfeld, unsigned int*, Spielfeld, Spielfeld, Evolution, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int, Special_Fields, Spielfeld, unsigned int*, Growth_Player*, Single_option_representives);
+
+
+void Having_too_much (unsigned int*, unsigned int, unsigned int, unsigned int, unsigned int, Evolution, Limits, unsigned int, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int, unsigned int, unsigned int, Spielfeld, Special_Fields, unsigned*, unsigned int);	//checklist
+
+#endif
