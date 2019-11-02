392662f840c42a78f63d0b3f26e2e05e59a78b93
Praefi <45772472+Praefi@users.noreply.github.com>
2019-11-03 00:51:47 +0100

    Create Growth_all_show.h
    spliting topics

---
+++ Growth_all_show.h
@@ -0,0 +1,36 @@
+
+#ifndef GROWTH_ALL_SHOW_H
+#define GROWTH_ALL_SHOW_H
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void show_field (unsigned int, unsigned int*, Spielfeld, Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int, Growth_Player*, unsigned int, Special_Fields);	//checklist
+
+
+void show_whose_turn (unsigned int, unsigned int, unsigned int*, Growth_Player*, unsigned int*, unsigned int);
+void show_statistics (unsigned int, unsigned int, Spielfeld, unsigned int*, unsigned int, unsigned int*, unsigned int*, unsigned int*, unsigned int*, unsigned int, Quidditch_setup*);	//checklist
+void show_options_of_actions (unsigned int, unsigned int*, unsigned int);
+void show_the_numbers (unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
+
+
+void show_figures ();
+
+
+void show_abilities (unsigned int*);
+
+
+
+
+
+
+
+
+#endif
