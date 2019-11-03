17ae6efc37e4d62aed480fadc6b51a7ca266dd54
Praefi <45772472+Praefi@users.noreply.github.com>
2019-11-03 23:00:46 +0100

    Update Growth_all_Def.c
    some bugfixes caused by the new structure

--- Growth_all_Def.c
+++ Growth_all_Def.c
@@ -78,65 +78,6 @@ unsigned int Nachfolger (unsigned int geben, unsigned int number_of_players) {
 	return nachher;
 }
 
-void About_the_game (unsigned int gamemode_played, unsigned int geben, Limits limits, unsigned int number_of_players, unsigned int freq, unsigned int w, unsigned int d, unsigned int e) {	//checklist
-
-	if (gamemode_played == Classic) {
-		printf("	How to win?   Own a square on your last line or force your opponents to lose all his/her ones.\n");
-	} else if (gamemode_played == Collect) {
-		printf("	How to win?   Be at 2 of the 3 targets with three of your squares near-by.\n");
-	} else if (gamemode_played == Contact) {
-		printf("	How to win?   Take over all your opponents' squares by winning Contact-battles.\n");
-		printf("	Contact-battles?  All near-by squares in a row with the Contact-square build a chain, beat your opponents in the length.\n");
-	} else if (gamemode_played == Fall) {
-		printf("	How to win?   Get more points than your opponents.\n");
-		printf("	Note:	Touch the #-square in the near-by to move it to the opposite side of touch. \n");
-	} else if ((gamemode_played == Fight)||(gamemode_played == Duell)) {
-		printf("	How to win?   Force your opponents to lose all his/her squares.\n");
-	} else if (gamemode_played == Hunt) {
-		if (geben == 1) {
-			printf("	How to win?   Protect your heart-block in a way that no enemy can touch it.\n");
-		} else {
-			printf("	How to win?   Touch the heart-block of the hunted one before it disappears.\n");
-		}
-	} else if (gamemode_played == Race) {
-		printf("	How to win?   Reach the end of the field or force your opponents to lose all his/her ones.\n");
-		printf("	Note:	Every %u turn(s) in total the #-line will go ahead and destroys everything in it's way.\n Also you are not allowed to cross the mid-line. \n", freq);
-	} else if (gamemode_played == Rain) {
-		printf("	How to win?   Reach [1][7]");
-		for (unsigned int p=2; p<=number_of_players; p++) {
-			printf(" or [1][%u]", (p-1)*7);
-		}
-		printf(" of the field or force your opponents to lose all his/her squares.\n");
-		printf("	Note:	Falling down the #-squares will push, destroy, explode, or teleport, transform into yours by contact. \n");
-	} else if ((gamemode_played == Arena)||(gamemode_played == Ulcer)) {	//10
-		printf("	How to win?   Force your opponents to lose all his/her squares while keeping yours alive by using your special abilities.\n");
-	} else if (gamemode_played == Dynamic) {
-		printf("	How to win?   Push the ball towards your opponent's side.\n");
-	} else if (gamemode_played == Survive) {
-		printf("	How to win?   Stay alive by avoiding traps, waves and bombs.\n");
-	} else if (gamemode_played == Sand) {
-		printf("	How to win?   Reach the topline, your squares keep falling.\n");
-	} else if (gamemode_played == Quidditch) {
-		printf("	How to win?   Get more points as your opponent. Every goal with a Quaffel worths 10 points, catching the Schnatz gives you 150 points and ends the game.\n");
-		printf("	Note:	Teammembers can move through changing position with one of your squares in its reach.\n	In the Near-by of objects, no living square is going to die.\n	In the Near-by of a Jaeger/Hueter the Quaffel is possessed and follows the movements of it's owner or can be thrown.\n	Passing this area the Quaffel stops during it's throw.\n \n If a Sucher flys through the Schnatz, it is caught.\n \n	If a Klatscher hits a target, the position of the target will change.\n ");
-	}
-	printf("	Surrounding:	The 8 squares around another, at the edge 5, in the corners 3, are called #surrounding. \n");
-	printf("	near-by:	The 4 squares around another, at the edge 3, in the corners 2, are called #near-by. \n");
-	printf("	Standard actions:	(After your choice of number and not influencing each other) \n	-Development:	If it is your turn and a free square has exactly %u of yours surrounding it, you will own it.\n", w);
-	printf("	-Losses:  If it is your turn and a square of yours is surrounded by less than %u or more than %u of yours,\n			it will be set free.\n", d, e);	//10
-	if ((gamemode_played == Hunt)&&(geben == 1)) {	//If geben == 1
-		printf("	Limits:   Your development is limited by %u per round, your units in total by %u per round. \n", (limits.new+(number_of_players-3)), (limits.at_all+(2*(number_of_players-3))));
-	} else if ((gamemode_played == Ulcer)||(gamemode_played == Survive)) {
-		printf("	Limits:   This gamemode is unlimited. \n");
-	} else if (gamemode_played == Contact) {
-		printf("	Limits:   Your development is limited by %u per round. \n", limits.new);
-	} else {
-		printf("	Limits:   Your development is limited by %u per round, your units in total by %u per round. \n", limits.new, limits.at_all);
-	}
-
-}
-
-
 
 void ges_counter (Spielfeld Field, unsigned int m, unsigned int n, unsigned int* ges, unsigned int number_of_players) {
 	for (unsigned int i=1; i<m-1; i+=1){
