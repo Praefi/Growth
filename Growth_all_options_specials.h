e7d5e8502d52d2eeec5a0b6464179bb095264d46
Praefi <45772472+Praefi@users.noreply.github.com>
2019-11-13 22:17:36 +0100

    Update Growth_all_options_specials.h
    see Growth_all.c

--- Growth_all_options_specials.h
+++ Growth_all_options_specials.h
@@ -8,26 +8,31 @@
 #include <math.h>
 #include <ctype.h>
 
+// #define VERBOSE
+// #define Contact_mistake_search
+// #define Quidditch_mistake_search
+
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-void realize_modifications (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, Spielfeld, Spielfeld, Special_Fields, Special_Fields, Spielfeld, unsigned int*, unsigned int*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int*, Growth_Player*, Special_Fields, unsigned int, Single_option_representives, Realize_modifications_variables*, Limits, unsigned int);
+void realize_modifications (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, Spielfeld, Spielfeld, Special_Fields, Special_Fields_Collector*, Spielfeld, unsigned int*, unsigned int*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int*, Growth_Player*, Special_Fields, unsigned int, Single_option_representives, Realize_modifications_variables*, Limits, unsigned int);
 
 
-void figure_check (Spielfeld, unsigned int, unsigned int, unsigned int, Growth_Player*, Special_Fields, Spielfeld, unsigned int, unsigned int);
+void figure_check (Spielfeld, unsigned int, unsigned int, unsigned int, Growth_Player*, Special_Fields_Collector*, Spielfeld, unsigned int, unsigned int);
 
 
-void inverted_organism (Spielfeld, unsigned int*, Spielfeld, Spielfeld, unsigned int, Spielfeld, unsigned int, Special_Fields, unsigned int, unsigned int, unsigned int, unsigned int*, Growth_Player*, Single_option_representives);
+void inverted_organism (Spielfeld, unsigned int*, Spielfeld, Spielfeld, unsigned int, Spielfeld, unsigned int, Special_Fields_Collector*, unsigned int, unsigned int, unsigned int, unsigned int*, Growth_Player*, Single_option_representives);
 void find_edges_of_organism_2dim (unsigned int*, Spielfeld, unsigned int, unsigned int, unsigned int);
 
 
-void addition_maker (Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields, unsigned int*);
-void projection_maker (Spielfeld, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
-void assassin_maker (unsigned int, Spielfeld, unsigned int*, Spielfeld, Spielfeld, unsigned int, Special_Fields, unsigned int, Special_Fields, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, Growth_Player*, Single_option_representives);
-void Avalanche_maker (Spielfeld, Spielfeld, Spielfeld, Special_Fields, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
-void Spreading_maker (Spielfeld, Spielfeld, Spielfeld, Special_Fields, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
-void Permutations_permutate_maker (Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int**, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields);
+void addition_maker (Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields_Collector*, unsigned int*);
+void projection_maker (Spielfeld, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields_Collector*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
+void assassin_maker (unsigned int, Spielfeld, unsigned int*, Spielfeld, Spielfeld, unsigned int, Special_Fields, unsigned int, Special_Fields_Collector*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, Growth_Player*, Single_option_representives);
+void avalanche_maker (Spielfeld, Spielfeld, Spielfeld, Special_Fields_Collector*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
+void spreading_maker (Spielfeld, Spielfeld, Spielfeld, Special_Fields_Collector*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
+void permutations_permutate_maker (Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int**, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields_Collector*);
+void Partition_compare_maker (Spielfeld, unsigned int, unsigned int, Spielfeld, unsigned int, Spielfeld, Special_Fields_Collector*, unsigned int, unsigned int);
 
 void translate_permutations_amounts_to_permutations_areas (Spielfeld, unsigned int, unsigned int, Collector);
 
