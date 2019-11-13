44b59d3f3676aeff6949cc2979d3c8a73480724d
Praefi <45772472+Praefi@users.noreply.github.com>
2019-11-13 22:17:47 +0100

    Update Growth_all_KI.h
    see Growth_all.c

--- Growth_all_KI.h
+++ Growth_all_KI.h
@@ -12,8 +12,12 @@
 #include <windows.h>
 #endif
 
-void synchronisation_Field_copy (Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields);
-void reset_Field_copy (Spielfeld, Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields);
+// #define VERBOSE
+// #define Contact_mistake_search
+// #define Quidditch_mistake_search
+
+void synchronisation_Field_copy (Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields_Collector*);
+void reset_Field_copy (Spielfeld, Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, unsigned int, Spielfeld, unsigned int, Special_Fields_Collector*);
 
 
 void synchronisation_ges_copy (unsigned int*, unsigned int, unsigned int*);
@@ -21,9 +25,9 @@ void reset_ges_copy (unsigned int*, unsigned int*, unsigned int);
 void ges_others_counter (unsigned int*, unsigned int*, unsigned int, unsigned int);
 
 
-void turn_of_KI_random (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, unsigned int*, Spielfeld, unsigned int, unsigned int, unsigned int, Special_Fields, Special_Fields, Spielfeld, unsigned int*, Growth_Player*, unsigned int, unsigned int, unsigned int* , unsigned int*, Limits, Single_option_representives, unsigned int, Realize_modifications_variables*, Special_Fields);
-void fill_KI_table_of_choice (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, unsigned int*, Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, Special_Fields, Special_Fields, Spielfeld, unsigned int*, Growth_Player*, unsigned int, unsigned int, unsigned int*, unsigned int*, unsigned int*, unsigned int*, Limits, Single_option_representives, unsigned int, Realize_modifications_variables*, Special_Fields);
-void set_KI_table_of_choice (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, unsigned int*, Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, Special_Fields, Special_Fields, Spielfeld, unsigned int*, Growth_Player*, unsigned int, unsigned int, unsigned int*, unsigned int*, unsigned int*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, Limits, Single_option_representives, unsigned int, Realize_modifications_variables*, Special_Fields);
+void turn_of_KI_random (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, unsigned int*, Spielfeld, unsigned int, unsigned int, unsigned int, Special_Fields, Special_Fields_Collector*, Spielfeld, unsigned int*, Growth_Player*, unsigned int, unsigned int, unsigned int* , unsigned int*, Limits, Single_option_representives, unsigned int, Realize_modifications_variables*, Special_Fields);
+void fill_KI_table_of_choice (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, unsigned int*, Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, Special_Fields, Special_Fields_Collector*, Spielfeld, unsigned int*, Growth_Player*, unsigned int, unsigned int, unsigned int*, unsigned int*, unsigned int*, unsigned int*, Limits, Single_option_representives, unsigned int, Realize_modifications_variables*, Special_Fields);
+void set_KI_table_of_choice (Quidditch_setup*, Moveable_objects_condition*, Moveable_objects_condition*, Moveable_objects_condition*, unsigned int*, Spielfeld, Spielfeld, unsigned int, unsigned int, unsigned int, Special_Fields, Special_Fields_Collector*, Spielfeld, unsigned int*, Growth_Player*, unsigned int, unsigned int, unsigned int*, unsigned int*, unsigned int*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, Limits, Single_option_representives, unsigned int, Realize_modifications_variables*, Special_Fields);
 void update_KI_table_of_choice (unsigned int*, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int*, unsigned int, unsigned int, unsigned int*, unsigned int, unsigned int, unsigned int*, unsigned int);
 
 
