6ddee14b7b1d75203be112cd7af7bd09e420c68c
Praefi <45772472+Praefi@users.noreply.github.com>
2019-11-13 22:16:57 +0100

    Update Growth_all_Vektor.h
    see Growth_all.c

--- Growth_all_Vektor.h
+++ Growth_all_Vektor.h
@@ -12,6 +12,10 @@
 #include <windows.h>
 #endif
 
+// #define VERBOSE
+// #define Contact_mistake_search
+// #define Quidditch_mistake_search
+
 Spielfeld Spielfeld_Create (unsigned int, unsigned int, unsigned int);		//mxn, m >= 6 (+2), n gerade, n >= 4 (+2)		Original: m = 10 (+2), n = 6 (+2)
 
 
@@ -21,6 +25,7 @@ int** int_2dim_Vektor_Create (unsigned int, unsigned int);
 unsigned int** unsigned_int_2dim_Vektor_Create (unsigned int, unsigned int);
 Moveable_objects_condition* Moveable_objects_condition_Vektor_Create (unsigned int);
 Realize_modifications_variables* Realize_modifications_variables_Vektor_Create (unsigned int);
+Special_Fields_Collector* Special_Fields_Collector_Vektor_Create (unsigned int);
 Quidditch_team_abilities* Quidditch_team_abilities_Vektor_Create (unsigned int);
 Quidditch_object_abilities* Quidditch_object_abilities_Vektor_Create (unsigned int);
 Quidditch_setup* Quidditch_setup_Vektor_Create (unsigned int);
@@ -35,6 +40,7 @@ void int_2dim_Vektor_Destroy (int**, unsigned int);
 void unsigned_int_2dim_Vektor_Destroy (unsigned int**, unsigned int);
 void Moveable_objects_condition_Vektor_Destroy (Moveable_objects_condition*);
 void Realize_modifications_variables_Vektor_Destroy (Realize_modifications_variables*);
+void Special_Fields_Collector_Vektor_Destroy (Special_Fields_Collector*);
 void Quidditch_team_abilities_Vektor_Destroy (Quidditch_team_abilities*);
 void Quidditch_object_abilities_Vektor_Destroy (Quidditch_object_abilities*);
 void Quidditch_setup_Vektor_Destroy (Quidditch_setup*);
