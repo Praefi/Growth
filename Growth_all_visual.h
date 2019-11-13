a728b257da512dadd99e52dde2d0b268b237f03a
Praefi <45772472+Praefi@users.noreply.github.com>
2019-11-13 22:16:36 +0100

    Update Growth_all_visual.h
    see Growth_all.c

--- Growth_all_visual.h
+++ Growth_all_visual.h
@@ -11,13 +11,14 @@
 #include <windows.h>
 #endif
 
+// #define VERBOSE
+// #define Contact_mistake_search
+// #define Quidditch_mistake_search
+
 void Square_color_interpretation (Growth_Player*, unsigned int, unsigned int, unsigned int);
 void get_colors (Growth_Player*, unsigned int, unsigned int, unsigned int);
 
 void set_terminal_color (Color);
 void set_player_color (Growth_Player*, unsigned int, Color);
-
-void opague_builder (Spielfeld, Special_Fields, unsigned int, unsigned int, unsigned int, unsigned int, Special_Fields, unsigned int, unsigned int);
-unsigned int list_of_possible_Field_values_from_one_to_ninethousand(unsigned int, unsigned int);	//checklist
 
 #endif
